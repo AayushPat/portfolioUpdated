faca20c6c733b5ae03d2e974e59fd1b9c521d12d
Aayush Patel <pate32ax@dukes.jmu.edu>
2026-01-07 13:15:17 -0500

    Add chart.js dependency for PrimeReact

--- vite.config.js
+++ vite.config.js
@@ -24,6 +24,6 @@ export default defineConfig({
   },
   // Optimize dependencies
   optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom']
+    include: ['react', 'react-dom', 'react-router-dom', 'chart.js/auto']
   }
 })
